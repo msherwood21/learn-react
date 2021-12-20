051494a05e4d3fd17b196326ece0f2f4fe3325b5
me <me@me.me>
2021-12-20 16:24:00 -0600

    - Updated to use non-standard port in case any other project is running - Updated compose file to use a bind mount for dev - Update Dockerfile to copy things into the correct directory - Added basic routing between web pages - Created dummy pages to test routing

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM node:17.2
 
 WORKDIR /opt/project
-COPY ./package.json /
+COPY ./package.json .
 RUN npm install
 
-COPY ./public /public
-COPY ./src /src
+COPY ./public public
+COPY ./src src
 
 EXPOSE 3000
 CMD npm run start
--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateResults from "./pages/CreateResults";
+import ViewResults from "./pages/ViewResults";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/create" element={<CreateResults />} />
+        <Route path="/view" element={<ViewResults />} />
+      </Routes>
     </div>
   );
 }
