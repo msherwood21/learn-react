bf5e23b4dd572435a2c224760cccee5440b9b3ab
Matthew Sherwood <msherwood21@gmail.com>
2021-12-20 19:05:49 -0600

    Added ability to route to unique ViewResults page

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,14 +1,27 @@
 import logo from "../logo.svg";
 import "../App.css";
-import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchResults() {
+  const navigate = useNavigate();
+  const raceRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    navigate("/view/" + raceRef.current.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Search Results</p>
-        <Link to="/view/1" >View Results</Link>
+        <form onSubmit={submitHandler}>
+          <label>Race Name</label>
+          <input type="text" required ref={raceRef} />
+          <button>Search Results</button>
+        </form>
       </header>
     </div>
   );
