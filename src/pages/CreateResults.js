20c94b7bbb1972b346b17e534aee362b0afa60d7
Matthew Sherwood <msherwood21@gmail.com>
2022-01-11 11:38:33 -0600

    - Converted CreateResults into a class component   - Name and # of drivers controls turned into controlled components - Behavior changed to clear the page instead of navigating away from the page on submit - On successful commit the page displays a prompt   - Timer added to remove the prompt after five seconds

--- src/pages/CreateResults.js
+++ src/pages/CreateResults.js
@@ -1,67 +1,114 @@
 import "../App.css";
-import React, { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import createResultsStyle from "./CreateResults.module.css";
+import propTypes from "prop-types";
+import React from "react";
 import { RaceResultsContext } from "../store/RaceResultsContext";
 import ResultsTable from "../components/results/ResultsTable";
 
-function CreateResults() {
-  const raceResults = useContext(RaceResultsContext);
-  const navigate = useNavigate();
-  const [name, setName] = useState("");
-  const [drivers, setDrivers] = useState([]);
+class CreateResults extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const submitHandler = () => {
-    raceResults.addRace({ name, drivers });
-    navigate("/");
+    this.state = {
+      name: props.initialResults.name,
+      driverCount: props.driverCount,
+      drivers: props.initialResults.drivers,
+      committed: props.committed,
+    };
+  }
+
+  submitHandler = () => {
+    this.context.addRace([{ name: this.state.name, drivers: this.state.drivers }]);
+    this.setState({ name: "", driverCount: 0, drivers: [], committed: true });
+    setTimeout(() => {
+      this.setState({ committed: false });
+    }, 5000);
   };
 
-  const resultHandler = (index, key, event) => {
-    const previousDrivers = drivers.slice(0, index);
-    const updatedResult = { ...drivers[index], [key]: event.target.value };
-    const nextDrivers = drivers.slice(index + 1);
-    setDrivers([...previousDrivers, updatedResult, ...nextDrivers]);
+  resultHandler = (index, key, event) => {
+    const previousDrivers = this.state.drivers.slice(0, index);
+    const updatedResult = { ...this.state.drivers[index], [key]: event.target.value };
+    const nextDrivers = this.state.drivers.slice(index + 1);
+
+    this.setState({
+      drivers: [...previousDrivers, updatedResult, ...nextDrivers],
+    });
   };
 
-  const driverCountHandler = ({ target: { value: driverCount } }) => {
-    const diff = driverCount - drivers.length;
+  driverCountHandler = ({ target: { value: driverCount } }) => {
+    const diff = driverCount - this.state.drivers.length;
     if (diff === 0) return;
-    if (diff < 0) return setDrivers(drivers.slice(0, driverCount));
+    if (diff < 0)
+      return this.setState({
+        driverCount: driverCount,
+        drivers: this.state.drivers.slice(0, driverCount),
+      });
 
     const newDriver = { driver: "", car: "", time: "" };
     const newRows = new Array(diff).fill(newDriver);
-    setDrivers([...drivers, ...newRows]);
+
+    this.setState({ drivers: [...this.state.drivers, ...newRows] }, () => {
+      this.setState({ driverCount: driverCount });
+    });
   };
 
-  return (
-    <div className="App">
-      <main>
-        <h1>Create Results</h1>
-        <div>
-          <label>Race Name: </label>
-          <input type="text" onChange={(e) => setName(e.target.value)} />
-        </div>
-        <div>
-          <label>Number of drivers: </label>
-          <input type="number" min="0" onInput={driverCountHandler} />
-        </div>
-        <div>
-          <p>
-            Enter the driver standings. Order is determined by entry position.
-          </p>
-          <ResultsTable
-            rowType={false}
-            raceResults={drivers}
-            onDriverNameChange={resultHandler}
-            onCarChange={resultHandler}
-            onTimeChange={resultHandler}
-          />
-        </div>
-        <div>
-          <button onClick={submitHandler}>Create Result</button>
-        </div>
-      </main>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <h1>Create Results</h1>
+          {this.state.committed && (
+            <h2 className={createResultsStyle.submitted}>Results committed!</h2>
+          )}
+          <div>
+            <label>Race Name: </label>
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
+          </div>
+          <div>
+            <label>Number of drivers: </label>
+            <input
+              type="number"
+              min="0"
+              value={this.state.driverCount}
+              onInput={this.driverCountHandler}
+            />
+          </div>
+          <div>
+            <p>Enter the driver standings. Order is determined by entry position.</p>
+            <ResultsTable
+              rowType={false}
+              raceResults={this.state.drivers}
+              onDriverNameChange={this.resultHandler}
+              onCarChange={this.resultHandler}
+              onTimeChange={this.resultHandler}
+            />
+          </div>
+          <div>
+            <button onClick={this.submitHandler}>Create Result</button>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
+
+CreateResults.propTypes = {
+  initialResults: propTypes.object.isRequired,
+};
+
+CreateResults.defaultProps = {
+  initialResults: {
+    name: "",
+    drivers: [],
+  },
+  driverCount: 0,
+  committed: false,
+};
+
+CreateResults.contextType = RaceResultsContext;
 
 export default CreateResults;
