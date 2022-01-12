4ae846f0a01e747bc4a0f1953415375bead15514
Matthew Sherwood <msherwood21@gmail.com>
2022-01-12 17:29:13 -0600

    Updated server to properly connect to the database - ES modules temporarily turned off to get things working

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,26 @@
-import * as http from "http";
+const http = require("http");
+const { Client } = require("pg");
 
-const hostname = '127.0.0.1';
+//- Database client
+//- Super crude sleep for now
+const stop = new Date().getTime() + 5000;
+while (new Date().getTime() < stop);
+const client = new Client({ user: "postgres", password: "test", host: "db" });
+client.connect();
+
+client.query("SELECT $1::text as message", ["Hello world!"], (err, res) => {
+  console.log(err ? err.stack : res.rows[0].message);
+  client.end();
+});
+
+//- API server
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
