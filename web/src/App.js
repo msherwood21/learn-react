d19d4c4234fb6411c5b704ab02f5ca84a4552e42
Matt Sherwood <msherwood21@gmail.com>
2022-01-18 15:25:58 -0600

    Removed the React context code from the web component

--- web/src/App.js
+++ web/src/App.js
@@ -5,20 +5,17 @@ import CreateResults from "./pages/CreateResults";
 import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import ViewResults from "./pages/ViewResults";
-import { RaceResultsContextProvider } from "./store/RaceResultsContext";
 
 function App() {
   return (
-    <RaceResultsContextProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/create" element={<CreateResults />} />
-          <Route path="/search" element={<SearchResults />} />
-          <Route path="/view/:id" element={<ViewResults />} />
-        </Route>
-      </Routes>
-    </RaceResultsContextProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/create" element={<CreateResults />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/view/:id" element={<ViewResults />} />
+      </Route>
+    </Routes>
   );
 }
 
