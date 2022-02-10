69e252511f099c699daaedaad51d306e46ee38ef
Matt Sherwood <msherwood21@gmail.com>
2022-02-10 12:02:17 -0600

    - Added database initialization from the server - Database bind mounts changed to a volume (was really no need for the bind mounts) - Removed example code and used it as a temporary space for upcoming db queries

--- server/src/data/racesProd.js
+++ server/src/data/racesProd.js
@@ -1,3 +1,5 @@
+const debug = require("debug")("express:db");
+
 //-
 //- Public API
 //-
@@ -5,19 +7,25 @@
 //- Determines if there are any objects with a matching name. Returns true or
 //  false.
 function isValidName(name) {
-  return true;
+  let result = true;
+
+  return result;
 }
 
 //- Determines if the ID matches any data in the backing store. Returns true or
 //  false.
 function isValidId(id) {
-  return false;
+  let result = false;
+
+  return result;
 }
 
 //- Determines if `data` matches the structure for a race object. Returns true
 //  or false.
 function isValidData(data) {
-  return false;
+  let result = false;
+
+  return result;
 }
 
 //- Commits data to the backing store. Returns the ID for looking up the data
@@ -25,18 +33,28 @@ function isValidData(data) {
 //- NOTE: Test version of this module is not thread safe. Ok for learning
 //    purposes, but don't depend on it.
 function commitData(data) {
-  return 0;
+  let result = 0;
+
+  return result;
 }
 
 //- Returns all races. An array of objects or an empty array is returned.
 function getAllData() {
-  return [];
+  let result = [];
+
+  reactPool.query(`SELECT * FROM races`).then((res) => {
+    res.rows.map((value) => console.log("-> " + value));
+  });
+
+  return result;
 }
 
 //- Returns the data associated with the ID from the backing store. Returns an
 //  object populated with race data or an empty object if there's an error.
 function getData(id) {
-  return {};
+  let result = {};
+
+  return result;
 }
 
 module.exports = { commitData, getData, getAllData, isValidData, isValidId, isValidName };
@@ -44,3 +62,84 @@ module.exports = { commitData, getData, getAllData, isValidData, isValidId, isVa
 //-
 //- Private API
 //-
+
+function checkDatabase() {
+  postgresPool = new Pool({ database: "postgres" });
+
+  postgresPool.query("SELECT datname FROM pg_database").then(
+    (value) => {
+      if (value.rows.find((value) => value.datname === "learn-react") === undefined) {
+        postgresPool
+          .query('CREATE DATABASE "learn-react"')
+          .then(() => {
+            debug("Database created");
+            checkDbEvent.emit("event");
+          })
+          .catch((err) => {
+            console.error("!! Failed to create the learn-react database");
+            throw err;
+          });
+      } else {
+        debug("Database already exists");
+        checkDbEvent.emit("event");
+      }
+    },
+    (err) => {
+      console.error("!! Querying all databases failed");
+      throw err;
+    }
+  );
+}
+
+function checkTable() {
+  reactPool = new Pool();
+
+  reactPool.connect().then(
+    () => {
+      reactPool.query(`SELECT EXISTS (SELECT FROM pg_tables WHERE tablename='races')`).then(
+        (res) => {
+          if (res.rows[0].exists) {
+            debug("Table already exists");
+          } else {
+            reactPool.query(`CREATE TABLE races(id bigint, name text, drivers jsonb)`).then(
+              () => {
+                debug("Table created");
+              },
+              (err) => {
+                console.error("!! Table creation failed");
+                throw err;
+              }
+            );
+          }
+        },
+        (err) => {
+          console.error("!! Selecting the table failed");
+          throw err;
+        }
+      );
+    },
+    (err) => {
+      console.error("!! Connection to database failed");
+      throw err;
+    }
+  );
+}
+
+//-
+//- Module Setup
+//-
+
+const { Pool } = require("pg");
+const EventEmitter = require("events");
+
+const checkDbEvent = new EventEmitter();
+let postgresPool = null;
+let reactPool = null;
+
+checkDbEvent.on("event", () => {
+  debug("Closing the postgres pool");
+  postgresPool.end();
+  checkTable();
+});
+
+checkDatabase();
--- server/src/example.js
+++ server/src/example.js
@@ -1,28 +1,17 @@
-const http = require("http");
-const { Client } = require("pg");
-
-//- Database client
-//- Super crude sleep for now
-const stop = new Date().getTime() + 5000;
-while (new Date().getTime() < stop);
-const client = new Client({ user: "postgres", password: "test", host: "db" });
-client.connect();
-
-client.query("SELECT $1::text as message", ["Hello world!"], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message);
-  client.end();
-});
-
-//- API server
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//- Startup
+//  - Create connection to the `postgres` database
+//    Is `learn-react` database created? -> SELECT datname FROM pg_database;
+//      If not, create the database -> CREATE DATABASE "learn-react"
+//  - Close the connection to the `postgres` database
+//  - Create connection to the `learn-react` database
+//  - Is the `races` table created?
+//      If not, create the table -> CREATE TABLE races(bigint id, text name, jsonb drivers);
+//- Shutdown
+//  - Close connection to the `learn-react` database
+//- Get all races
+//  - SELECT * FROM races;
+//- Get a race
+//  - SELECT * FROM races WHERE id = `id`;
+//- Add a race
+//  - INSERT INTO races (id, name, drivers) VALUES (`id`, '`name`', `drivers`);
+//    NOTE: Postgres requires all object properties have double quotes around them.
